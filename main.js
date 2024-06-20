d5cffdb4d77e5ce3c6a3f74d14fc46c073675d65
Frominor <miha.mosp@yandex.ru>
2024-06-20 13:20:00 +0300

    final

--- main.js
+++ main.js
@@ -2,96 +2,237 @@ const {
   Bot,
   GrammyError,
   HttpError,
-  Keyboard,
   InlineKeyboard,
+  InputFile,
+  Keyboard,
 } = require("grammy");
-const { GetRandomQuestion, GetRightAnswer, FindErrorCode } = require("./utils");
+const { FindErrorCode } = require("./utils");
+const fs = require("fs");
+let isPdfCall = false;
+let isRegister = false;
+let isErrorFound = false;
 require("dotenv").config();
 const bot = new Bot(process.env.TELEGABOT_KEY);
-FindErrorCode("5@AEE00");
-bot.command("start", async (ctx) => {
-  const ThemeKeyboard = new Keyboard()
-    .text("HTML")
-    .text("CSS")
-    .text("javascript")
-    .text("React")
-    .resized();
-  await ctx.reply("Выберите тему", {
-    reply_markup: ThemeKeyboard,
-  });
-});
-bot.hears(["HTML", "CSS", "javascript", "React"], async (ctx) => {
-  const question = GetRandomQuestion(ctx.message?.text);
-  let inlineKeyboard;
-  if (question.hasOptions) {
-    const buttonRow = question?.options?.map((item) => {
-      return [
-        InlineKeyboard.text(
-          item.text,
-          JSON.stringify({
-            type: `${ctx.message.text}`,
-            isCorrect: item.isCorrect,
-            questionId: question.id,
-          })
-        ),
-      ];
-    });
-    inlineKeyboard = InlineKeyboard.from(buttonRow);
+bot.command("registerzapros", async (ctx) => {
+  let registeusers = fs.readFileSync("ZaprosToRegisterUsers.json");
+  let Users = JSON.parse(registeusers);
+  if (ctx.from.id == 954367118) {
+    for (let k of Users) {
+      await ctx.reply(`${k.name}`, {
+        reply_markup: new InlineKeyboard()
+          .text("Да", JSON.stringify({ id: k.id, add: true }))
+          .text("Нет", JSON.stringify({ id: k.id, add: false })),
+      });
+    }
+    return;
   } else {
-    inlineKeyboard = new InlineKeyboard().text(
-      "Узнать ответ",
-      JSON.stringify({
-        type: ctx.message?.text,
-        questionId: question.id,
-      })
-    );
+    await ctx.reply("Этот функционал доступен только администратору");
   }
-  await ctx.reply(`${question?.text}`, {
-    reply_markup: inlineKeyboard,
-  });
-});
-bot.on("callback_query:data", async (ctx) => {
-  const question = GetRandomQuestion(
-    JSON.parse(ctx.update.callback_query?.data).type
-  );
-  const parsedDatas = JSON.parse(ctx.update.callback_query?.data);
-  const rightAnswer = GetRightAnswer(parsedDatas.type, parsedDatas.questionId);
-  let inlineKeyboard;
-  if (question.hasOptions) {
-    const buttonRow = question.options.map((item) => {
-      return [
-        InlineKeyboard.text(
-          item.text,
-          JSON.stringify({
-            isCorrect: item.isCorrect,
-            type: JSON.parse(ctx.update.callback_query?.data).type,
-            questionId: question.id,
-          })
-        ),
-      ];
-    });
-    inlineKeyboard = InlineKeyboard.from(buttonRow);
-  } else {
-    inlineKeyboard = new InlineKeyboard().text(
-      "Узнать ответ",
-      JSON.stringify({
-        type: JSON.parse(ctx.update.callback_query?.data).type,
-        questionId: question.id,
-      })
-    );
+  if (Users.length == 0) {
+    await ctx.reply("Пусто");
+    return;
   }
-  if (parsedDatas.isCorrect) {
-    await ctx.reply("Верно");
-  } else if (parsedDatas.isCorrect == false) {
-    await ctx.reply(`Неверно,правильный ответ-${rightAnswer}`);
-  }
-  if (parsedDatas?.isCorrect == undefined) {
-    await ctx.reply(rightAnswer);
-  }
-  await ctx.reply(`${question.text}`, { reply_markup: inlineKeyboard });
 });
 
-bot.api.setMyCommands([{ command: "start", description: "Начать" }]);
+bot.command("start", async (ctx) => {
+  let OptionsKeyboard;
+  const users = JSON.parse(fs.readFileSync("Users.json"));
+  if (users.find(({ id }) => id == ctx.from.id)) {
+    OptionsKeyboard = new Keyboard()
+      .text("База знаний")
+      .row()
+      .text("Код ошибки")
+      .row()
+      .resized();
+    await ctx.reply("Главное меню", { reply_markup: OptionsKeyboard });
+  } else {
+    OptionsKeyboard = new Keyboard().text("Регистрация").row().resized();
+    await ctx.reply("Зарегистрируйтесь,чтобы получить доступ к боту", {
+      reply_markup: OptionsKeyboard,
+    });
+  }
+});
+bot.hears("База знаний", async (ctx) => {
+  isErrorFound = false;
+  isRegister = false;
+  let OptionsKeyboard = new Keyboard()
+    .text("В главное меню")
+    .row()
+    .text("Инструкции от банков")
+    .resized()
+    .text("Устройства")
+    .row()
+    .text("Базовые инструкции ")
+    .resized()
+    .text("Проектные работы")
+    .row()
+    .row();
+  await ctx.reply("Меню", {
+    reply_markup: OptionsKeyboard,
+  });
+});
+bot.hears("Инструкции от банков", async (ctx) => {
+  let OptionsKeyboard = new Keyboard()
+    .text("В главное меню")
+    .row()
+    .text("Сбербанк")
+    .resized()
+    .text("ВТБ")
+    .row()
+    .text("Райфайзен")
+    .resized()
+    .text("Альфа")
+    .row()
+    .text("Зенит Банк")
+    .resized()
+    .text("Совкомбанк")
+    .row()
+    .text("Тинькофф")
+    .resized()
+    .text("Магнит КСО")
+    .row()
+    .text("Открытие")
+    .resized()
+    .text("Газпромбанк")
+    .row();
+  await ctx.reply("Выберите банк", {
+    reply_markup: OptionsKeyboard,
+  });
+});
+bot.hears("В главное меню", async (ctx) => {
+  let OptionsKeyboard = new Keyboard()
+    .text("База знаний")
+    .row()
+    .text("Код ошибки")
+    .row()
+    .resized();
+  await ctx.reply("Главное меню", {
+    reply_markup: OptionsKeyboard,
+  });
+});
+bot.hears("Устройства", async (ctx) => {
+  let OptionsKeyboard = new Keyboard()
+    .text("В главное меню")
+    .row()
+    .text("MX 8600")
+    .resized()
+    .text("MX8600S")
+    .row()
+    .text("MX8600SB 19/32")
+    .resized()
+    .text("MX8600ST")
+    .row()
+    .text("TCR")
+    .resized()
+    .text("MX9100SB")
+    .row()
+    .resized();
+  await ctx.reply("Выберите устройство", {
+    reply_markup: OptionsKeyboard,
+  });
+});
+bot.hears("Регистрация", async (ctx) => {
+  isErrorFound = false;
+  isPdfCall = false;
+  await ctx.reply("Введите ваше ФИО");
+  isRegister = true;
+});
+bot.hears("Код ошибки", async (ctx) => {
+  isPdfCall = false;
+  isRegister = false;
+  await ctx.reply("Введите код ошибки");
+  isErrorFound = true;
+});
+bot.on("callback_query:data", async (ctx) => {
+  const users = JSON.parse(fs.readFileSync("ZaprosToRegisterUsers.json"));
+  let infoaboutusers = JSON.parse(ctx.update.callback_query.data);
+  if (infoaboutusers.add) {
+    const users = JSON.parse(fs.readFileSync("Users.json"));
+    if (users.find(({ id }) => id === infoaboutusers.id)) {
+    } else {
+      users.push({ id: infoaboutusers.id });
+      fs.writeFileSync("Users.json", JSON.stringify(users));
+      const filteredusers = JSON.stringify(
+        users.filter((item) => item.id !== infoaboutusers.id)
+      );
+      fs.writeFileSync("ZaprosToRegisterUsers.json", filteredusers);
+      await ctx.answerCallbackQuery();
+
+      await ctx.reply(
+        `Пользователь ${infoaboutusers.id} успешно зарегистрирован`
+      );
+      return;
+    }
+  } else {
+    const filteredusers = JSON.stringify(
+      users.filter((item) => item.id !== infoaboutusers.id)
+    );
+    fs.writeFileSync("ZaprosToRegisterUsers.json", filteredusers);
+    await ctx.reply(`Заявка пользователя -${infoaboutusers.id} была отклонена`);
+  }
+});
+bot.on("message", async (ctx) => {
+  if (isPdfCall) {
+    await ctx.replyWithDocument(new InputFile("./tests.pdf"));
+    ctx.answerCallbackQuery();
+    isPdfCall = false;
+    return;
+  }
+  if (isRegister) {
+    let rawdata = JSON.parse(fs.readFileSync("ZaprosToRegisterUsers.json"));
+    let RegisterUsers = JSON.parse(fs.readFileSync("Users.json"));
+    for (let k of rawdata) {
+      if (k.id === ctx.from.id) {
+        await ctx.reply("Такой пользователь уже ожидает регистрации ");
+      }
+    }
+    for (let k of RegisterUsers) {
+      if (k.id == ctx.from.id) {
+        await ctx.reply("Такой пользователь уже зарегистрирован");
+        return;
+      }
+    }
+    rawdata.push({
+      name: ctx.update.message.text.replace(/ +/g, " "),
+      id: ctx.from.id,
+    });
+    let data = JSON.stringify(rawdata);
+    fs.writeFileSync("ZaprosToRegisterUsers.json", data);
+    await ctx.reply("Вас внесли в список на авторизацию.");
+
+    ctx.answerCallbackQuery();
+    return;
+  }
+  if (isErrorFound) {
+    const Error = FindErrorCode(ctx.update.message.text.toUpperCase());
+    if (Error) {
+      if (Error.isCritical) {
+        await ctx.reply(
+          `<b>Обратите внимание,эта критическая ошибка!'⚠️'</b> 
+Описание ошибки-${Error.ErrorDescription} `,
+          {
+            parse_mode: "HTML",
+          }
+        );
+        await ctx.reply(`Как исправить-${Error.HowToFixError}`);
+      } else {
+        await ctx.reply(`${Error.ErrorDescription}`);
+        await ctx.reply(`Как исправить-${Error.HowToFixError}`);
+      }
+    } else {
+      await ctx.reply(
+        "Такого кода нет,проверьте правильность кода и попробуйте еще раз"
+      );
+    }
+    ctx.answerCallbackQuery();
+    return;
+  }
+});
+
+bot.api.setMyCommands([
+  { command: "registerzapros", description: "Запросы на регистрацию" },
+  { command: "start", description: "Старт " },
+]);
 bot.catch((err) => {
   const ctx = err.ctx;
   console.error(`Error while handling update ${ctx.update.update_id}:`);
@@ -104,9 +245,5 @@ bot.catch((err) => {
     console.error("Unknown error:", e);
   }
 });
-bot.command("test", async (ctx) => {
-  await ctx.replyWithPhoto(
-    "https://yandex.ru/images/search?from=tabbar&img_url=https%3A%2F%2Fsun9-72.userapi.com%2Fimpf%2Fc636428%2Fv636428147%2F470dd%2FMeGQy2dlsns.jpg%3Fsize%3D1280x800%26quality%3D96%26sign%3D88a4b3b546ab0154da34d50367b2cd5e%26c_uniq_tag%3DMdkz6RcA0_JyO0W6s-UnkmVSmM2yc0TFgZR4D4RvkbI%26type%3Dalbum&lr=191&pos=5&rpt=simage&text=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D1%83%D0%B8"
-  );
-});
+
 bot.start();
--- utils.js
+++ utils.js
@@ -1,22 +1,5 @@
-const questions = require("./questions.json");
 const TerminalErrors = require("./TerminalErrors.json");
-const GetRandomQuestion = function (QuestionType) {
-  const topic = questions[QuestionType.toLowerCase()];
-  console.log("dasda", topic[Math.floor(Math.random() * topic.length)]);
-  return topic[Math.floor(Math.random() * topic.length)];
-};
-const GetRightAnswer = function (topic, questionId) {
-  if (questions[topic.toLowerCase()][questionId - 1]?.hasOptions) {
-    const questionsGroup = questions[topic.toLowerCase()][
-      questionId - 1
-    ].options.filter((item) => item.isCorrect);
-    return questionsGroup[0].text;
-  } else {
-    return questions[topic.toLowerCase()][questionId - 1].answer;
-  }
-};
 const FindErrorCode = function (code) {
-  const ErrorCode = TerminalErrors.find((item) => item.ErrorCode == code);
-  console.log(ErrorCode);
+  return TerminalErrors.find((item) => item.ErrorCode == code);
 };
-module.exports = { GetRandomQuestion, GetRightAnswer, FindErrorCode };
+module.exports = { FindErrorCode };
